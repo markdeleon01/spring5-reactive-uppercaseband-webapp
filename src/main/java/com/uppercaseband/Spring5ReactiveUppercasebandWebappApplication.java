c24487c77125868b92f785ceb57069acc98791f0
markyMARK <>
2018-11-27 03:45:05 -0500

    Initial commit - Reactive version:  Gradle, MongoDB dependencies, tests

---
+++ src/main/java/com/uppercaseband/Spring5ReactiveUppercasebandWebappApplication.java
@@ -0,0 +1,12 @@
+package com.uppercaseband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReactiveUppercasebandWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5ReactiveUppercasebandWebappApplication.class, args);
+	}
+}
